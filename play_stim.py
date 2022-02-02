df13ad7bd61d183593ea12101ef918e7b375c3e5
Letitia Ho <letitiayhho@gmail.com>
2022-02-02 15:53:36 -0600

    Add block number, increase time of each stim and ISI

--- play_stim.py
+++ play_stim.py
@@ -6,28 +6,35 @@ from psychtoolbox import GetSecs, WaitSecs
 import numpy as np
 import os.path
 import csv
+import time
+
+start_time = time.time()
 
 TEST_MODE = False
 
-trials = 20
+trials = 2000
 min_freq = 40
 max_freq = 254
 
 # init device to send TTL triggers
 #marker = EventMarker()
 
-# set the seed to the subject number so trial order can be recreated
+# ask for subject and block number
 sub_num = input("Input subject number: ")
-log = "subj_" + sub_num + ".log"
-sub_num = int(sub_num)
-np.random.seed(sub_num)
+block_num = input("Input block number: ")
 
 # count trial progress in log file
+log = "subj_" + sub_num + "_block_" + block_num + ".log"
 if not os.path.isfile(log): # create log file if it doesn't exist
     with open(log, 'w', newline='') as f:
         writer = csv.writer(f)
         writer.writerow(['trial', 'freq', 'marker'])
 trial_count = sum(1 for line in open(log))
+
+# set subject number and block as seed
+seed = int(sub_num + "0" + block_num)
+print("Seed: " + str(seed))
+np.random.seed(seed)
 
 # start the experiment
 WaitSecs(5.)
@@ -39,7 +46,7 @@ for i in range(trial_count, trials + 1):
     else:
         freq = np.random.randint(min_freq, max_freq)
         freq = freq - (freq%2) # get only even numbered freqs for tags
-    snd = Sound(freq, secs = 0.1)
+    snd = Sound(freq, secs = 0.2)
 
     # schedule sound
     now = GetSecs()
@@ -54,7 +61,8 @@ for i in range(trial_count, trials + 1):
         writer.writerow([i, freq, marker])
 
     # add jitter between trials
-    WaitSecs(0.1+np.random.uniform(0, 0.1))
+    WaitSecs(0.2+np.random.uniform(0, 0.1))
 
 #marker.close()
 print("Done.")
+print("--- %s seconds ---" % (time.time() - start_time))
