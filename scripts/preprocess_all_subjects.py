03713c199abbb5594265879bffc9bc55f24f43e3
Letitia Ho <letitiayhho@uchicago.edu>
2022-05-19 15:45:01 -0500

    Simplify preprocess_all_subjects using iter_fpaths()

--- scripts/preprocess_all_subjects.py
+++ scripts/preprocess_all_subjects.py
@@ -2,35 +2,13 @@
 
 import subprocess
 import sys
-from bids import BIDSLayout
-from typing import Tuple, Iterator
-from util.io.preprocessing import *
+from util.io.iter_fpaths import *
 
 def main() -> None:
-    # CONSTANTS
     BIDS_ROOT = '../data/bids'
 
-    # Parse BIDS directory
-    layout = BIDSLayout(BIDS_ROOT)
-    subjects = layout.get_subjects()
-    tasks = layout.get_tasks()
-    runs = layout.get_runs()
-    print(subjects, tasks, runs)
-
-    KeyType = Tuple[str, str, str]
-
-    def fpaths() -> Iterator[KeyType]:
-        for sub in subjects:
-            for task in tasks:
-                for run in runs:
-                    run = str(run) # layout.get_runs() doesn't return strings
-                    key = (sub, task, run)
-                    yield key
-
-    for (sub, task, run) in fpaths():
-        bids_path = get_bids_path(BIDS_ROOT, sub, task, run)
-        if bids_path.fpath.is_file():
-            	subprocess.check_call("sbatch ./preprocess.py %s %s %s" % (sub, task, run), shell=True)
+    for (fpath, sub, task, run) in iter_fpaths(BIDS_ROOT):
+        subprocess.check_call("sbatch ./preprocess.py %s %s %s %s" % (fpath, sub, task, run), shell=True)
 
 if __name__ == "__main__":
     if len(sys.argv) != 1:
