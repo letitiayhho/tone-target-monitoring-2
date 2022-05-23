add3612942c64ce3b0d3fbca2101afd54bf79ea5
Letitia Ho <letitiayhho@gmail.com>
2022-05-23 10:09:20 -0500

    Add run number

--- scripts/decode_one_subject.py
+++ scripts/decode_one_subject.py
@@ -8,15 +8,15 @@ def main(test_sub) -> None:
     BIDS_ROOT = '../data/bids'
 
     for (fpath, sub, task, run) in iter_fpaths(BIDS_ROOT):
-        if sub != test_sub:
+        if sub != test_sub and run != test_run:
             continue
         log = open("logs/decoder_test_" + "test_sub" + ".log", 'w')
         subprocess.check_call("./decoder.py %s %s %s %s" % (fpath, sub, task, run), shell=True, stdout = log)
-        break
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 3:
         print(__doc__)
         sys.exit(1)
     test_sub = sys.argv[1]
+    test_run = sys.argv[2]
     main(test_sub)
