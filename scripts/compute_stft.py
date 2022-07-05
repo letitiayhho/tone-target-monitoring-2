37b973bd1b6f234349148bafc30ae142ad9d9146
Letitia Ho <letitiayhho@midway2-login2.rcc.local>
2022-07-05 10:51:08 -0500

    Add jupyter

---
+++ scripts/compute_stft.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import sys
+import mne
+
+def main(fpath, sub, task, run):
+    DERIV_ROOT = '../data/bids/derivatives'
+    
+# load data
+
+# compute power with stft
+
+# save power in '../data/bids/derivatives/stft' 
