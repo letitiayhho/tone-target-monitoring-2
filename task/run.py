377373f892aec21b47eee3cab379bd1686142c50
Jared Bendavid <108893363+jbendavid@users.noreply.github.com>
2022-08-21 12:23:10 -0500

    Integrates run_training
    Integrates run_training; if block 0, score_needed is set to 3 else score set to 18

--- task/run.py
+++ task/run.py
@@ -10,6 +10,7 @@ SUB_NUM = input("Input subject number: ")
 BLOCK_NUM = input("Input block number: ")
 WIN = visual.Window([800,600], monitor="testMonitor", units="deg")
 
+    
 #marker = EventMarker()
 
 # open log file
@@ -23,6 +24,14 @@ print(f"seq_num: {seq_num}")
 SEED = int(SUB_NUM + "0" + BLOCK_NUM + str(seq_num))
 print("Current seed: " + str(SEED))
 random.seed(SEED)
+
+#display instructions if training block
+if BLOCK_NUM == "0": 
+    SCORE_NEEDED = 3
+    instructions(WIN)
+else: 
+    SCORE_NEEDED =18
+
 
 # play sequences until SCORE_NEEDED is reached
 while score < SCORE_NEEDED:
