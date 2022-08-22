a96959b2b00701302930e3165efc2d6fdf2b0a36
Letitia Ho <letitiayhho@uchicago.edu>
2022-08-22 18:27:17 -0500

    Add script to test EEG timing and check stim signals

---
+++ task/timing-test.py
@@ -0,0 +1,27 @@
+from functions import *
+from events import EventMarker
+from psychtoolbox import GetSecs, WaitSecs
+from psychopy.sound.backend_ptb import SoundPTB as Sound
+import random
+
+FREQS = [130, 200, 280]
+TONES = 1000
+
+marker = EventMarker()
+
+for i in range(TONES):
+
+    index = random.randint(0, len(FREQS)-1)
+    freq = FREQS[index]
+    mark = index + 1
+    snd = Sound(freq, secs = TONE_LEN)
+
+    now = GetSecs()
+    snd.play(when = now + 0.1)
+    WaitSecs(0.1)
+    marker.send(mark) CHANGE THIS
+    WaitSecs(TONE_LEN - 0.1)
+
+    WaitSecs(0.1)
+
+print("Done :^)")
