a3edbb87bb51f4b7fd96eb43d66cc43938d666cd
Letitia Ho <letitiayhho@gmail.com>
2022-09-01 08:00:21 -0500

    Fix some formatting

--- task/functions.py
+++ task/functions.py
@@ -80,13 +80,13 @@ def fixation(WIN):
     WIN.flip()
     return(fixation)
 
-def hear_pitches(WIN, TONE_LEN, FREQS): 
+def hear_pitches(WIN, TONE_LEN, FREQS):
     p1 = Sound(FREQS[0], secs = TONE_LEN)
     p2 = Sound(FREQS[1], secs = TONE_LEN)
     p3 = Sound(FREQS[2], secs = TONE_LEN)
-    
-    p1_txt = visual.TextStim(WIN, 
-                            text = """You will be presented with random sequences of three tones in this task. You will now hear the three tones. Press 'enter' to hear the first tone.""", 
+
+    p1_txt = visual.TextStim(WIN,
+                            text = "You will be presented with random sequences of three tones in this task. You will now hear the three tones. Press 'enter' to hear the first tone.",
                             pos=(0.0, 0.0),
                             color=(1, 1, 1),
                             colorSpace='rgb')
@@ -95,13 +95,13 @@ def hear_pitches(WIN, TONE_LEN, FREQS):
     event.waitKeys(keyList = ['return'])
     p1.play()
     core.wait(1)
-    
-    p2_txt = visual.TextStim(WIN, 
-                                text = "Press 'enter' to hear the second tone.", 
+
+    p2_txt = visual.TextStim(WIN,
+                                text = "Press 'enter' to hear the second tone.",
                                 pos=(0.0, 0.0),
                                 color=(1, 1, 1),
                                 colorSpace='rgb')
-    
+
     event.clearEvents(eventType = None)
     p2_txt.draw()
     WIN.flip()
@@ -125,12 +125,12 @@ def hear_pitches(WIN, TONE_LEN, FREQS):
 
 def instructions(WIN):
     instruction_text = visual.TextStim(WIN,
-                                      text = "In each trial of this experiment, one of the three tones you just heard will be randomly selected as the ‘target’ tone. You will be allowed to listen to the target tone as many times as you like. This target tone will then be played amidst a sequence of the other two tones. Your task is to count and remember how many times you hear the target tone in the sequence. Press ‘space’ for the remaining instructions…",
+                                      text = "In each trial of this experiment, one of the three tones you just heard will be randomly selected as the ‘target’ tone. You will be allowed to listen to the target tone as many times as you like. This target tone will then be played amidst a sequence of the other two tones. Your task is to count and remember how many times you hear the target tone in the sequence. Press 'space' for the remaining instructions…",
                                       pos=(0.0, 0.0),
                                       color=(1, 1, 1),
                                       colorSpace='rgb')
     instruction2_text = visual.TextStim(WIN,
-                                        text = "You will be asked how many times you heard the target tone at the end of a sequence and if you accurately report the number of target tones–or come close to the actual number of target tones–your score will increase by 1. To finish the task, you will have to reach a score of 18. Please ask your experimenter any questions you may have about the task. Press ‘space’ to continue…",
+                                        text = "You will be asked how many times you heard the target tone at the end of a sequence and if you accurately report the number of target tones–or come close to the actual number of target tones–your 'score' will increase by 1. To finish the task, you will have to reach a score of 18. Please ask your experimenter any questions you may have about the task. Press 'space' to continue…",
                                         pos=(0.0, 0.0),
                                         color=(1, 1, 1),
                                         colorSpace='rgb')
