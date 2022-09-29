1e78779ea561e4c7a5635441c8cf9f9cfbea994d
Letitia Ho <letitiayhho@uchicago.edu>
2022-09-29 11:31:15 -0500

    Don't recode event tags, get random captrak file if captrak file is missing

--- analysis/convert-to-bids.py
+++ analysis/convert-to-bids.py
@@ -9,6 +9,7 @@
 #SBATCH --output=logs/convert-to-bids_%j.log
 
 from mne_bids import BIDSPath, write_raw_bids, get_anonymization_daysback
+import random
 import pandas as pd
 import numpy as np
 import itertools
@@ -33,8 +34,8 @@ def main(fpath, sub, task, run) -> None:
         datatype = 'eeg',
         root = BIDS_DIR
     )
-#     if os.path.isfile(bids_path):
-#         sys.exit(f'File {bids_path} exists, skipping {fname}')
+    if os.path.isfile(bids_path):
+        sys.exit(f'File {bids_path} exists, skipping {fname}')
 
     # load data with MNE function for your file format
     fpath = os.path.join(RAW_DIR, fpath)
@@ -51,29 +52,32 @@ def main(fpath, sub, task, run) -> None:
     raw.rename_channels(mapping)
     raw.add_reference_channels(ref_channels = ['Cz'])
 
-    # drop extra 32 channels for subs 23, 24, 25
-    if sub in ['23', '24', '25']:
-        print(f"Dropping extra channels for sub {sub}!")
-        matches = [x for x in raw.ch_names if re.search('Ch', x)]
-        raw = raw.drop_channels(matches)
-    print(raw.ch_names)
-    n_chans = len(raw.ch_names)
+    # checks
     if n_chans != 65:
         sys.exit(f"Incorrect number of channels, there should be 65 (stim incl) channels, instead there are {n_chans} channels")
 
     # map channels to their coordinates
-    dig = mne.channels.read_dig_captrak(MAPS_DIR + 'subj-' + sub + '.bvct')
-    raw.set_montage(dig, on_missing = 'warn')
+    captrak_found = False
+    captrak_sub = sub
+    while not captrak_found:
+        captrak_path = MAPS_DIR + 'sub-' + captrak_sub + '.bvct'
+        if os.path.isfile(captrak_path):
+            print(f"Using captrak file from {captrak_sub}")
+            dig = mne. channels.read_dig_captrak(captrak_path)
+            raw.set_montage(dig, on_missing = 'warn')
+            captrak_found = True
+        else:
+            captrak_sub = random.randint(3, 30)
 
     # drop meaningless event name
     events, event_ids = mne.events_from_annotations(raw)
     events = events[events[:,2] != event_ids['New Segment/'], :]
 
     # rename events to their stimulus pitch
-    event_codes = events[:,2]
-    baseline_code = np.argmax(np.bincount(event_codes)) # the one with more trials
-    event_names = {1: '50', 2: '100', 3: '150', 4: '200', 5: '250'}
-    annot = mne.annotations_from_events(events, sfreq = raw.info['sfreq'], event_desc = event_names)
+    #event_codes = events[:,2]
+    #baseline_code = np.argmax(np.bincount(event_codes)) # the one with more trials
+    #event_names = {12: '50', 2: '100', 3: '150', 4: '200', 5: '250'}
+    annot = mne.annotations_from_events(events, sfreq = raw.info['sfreq'])
     raw.set_annotations(annot)
 
     # get range of dates the BIDS specification will accept
